0d4432768ff1bc23d70f92326600689e14474998
ryanpdx <rmedick@pdx.edu>
2020-02-24 17:53:01 -0800

    major overhaul of OD_helper.*

--- src/common/OD_helpers.c
+++ src/common/OD_helpers.c
@@ -1,27 +1,57 @@
 #include "CANopen.h"
-#include "CO_SDO.h" /* only for return codes*/
 #include "log_message.h"
 #include "OD_helpers.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <stdlib.h>
+
 
 extern CO_OD_entry_t CO_OD[CO_OD_NoOfElements];
 
-/******************************************************************************/
-uint16_t app_OD_find(uint16_t index){
-    /* Fast search in ordered Object Dictionary. If indexes are mixed, this won't work. */
-    /* If Object Dictionary has up to 2^N entries, then N is max number of loop passes. */
+
+void
+app_OD_configure(
+        uint16_t index,
+        CO_SDO_abortCode_t (*pODFunc)(CO_ODF_arg_t *ODF_arg),
+        void *object,
+        uint8_t *flags,
+        uint8_t flagsSize) {
+
+    CO_OD_configure(CO->SDO[0], index, pODFunc, object, flags, flagsSize);
+}
+
+
+void
+app_log_message(const char *app_name, int priority, const char *message) {
+    char *format = "";
+
+    // merge strings
+    strncat(format, app_name, strlen(app_name));
+    strcat(format, ": ");
+    strncat(format, message, strlen(message));
+
+    log_message(priority, format);
+}
+
+
+uint16_t
+app_OD_find(uint16_t index){
     uint16_t cur, min, max;
     const CO_OD_entry_t* object;
 
+    // Fast search algorithm
     min = 0U;
     max = CO_OD_NoOfElements - 1U;
     while(min < max){
         cur = (min + max) / 2;
         object = &CO_OD[cur];
-        /* Is object matched */
+        // Is object matched
         if(index == object->index){
             return cur;
         }
@@ -35,7 +65,7 @@ uint16_t app_OD_find(uint16_t index){
 
     if(min == max){
         object = &CO_OD[min];
-        /* Is object matched */
+        // Is object matched
         if(index == object->index){
             return min;
         }
@@ -45,154 +75,138 @@ uint16_t app_OD_find(uint16_t index){
 }
 
 
-/******************************************************************************/
-uint32_t app_readOD(uint16_t index, uint16_t subIndex, void *data, uint16_t *length){
+bool
+app_OD_read(uint16_t index, uint16_t sub_index, void *data, uint16_t *length) {
     CO_OD_entry_t* object = NULL;
-    int8_t *ODdata = NULL;
-    uint16_t ODlength;
-    uint16_t ODattribute;
+    int8_t *OD_data = NULL;
+    uint16_t OD_entry_num;
+    uint16_t OD_length;
+    uint16_t OD_attribute;
 
-    if(data == NULL) {
-        return CO_SDO_AB_NO_DATA; 
-    }
+    if(data == NULL)
+        return false; // No data pointer for return
 
-    /* get object location */
-    uint16_t entryNo = app_OD_find(index);
-    if(entryNo == 0xFFFE) {
-        return CO_SDO_AB_NOT_EXIST;  /* index not found */
-    }
-    
-    /* get object */
-    object = &CO_OD[entryNo];
+    // Get object location
+    OD_entry_num = app_OD_find(index);
+    if(OD_entry_num == 0xFFFE)
+        return false; // Index not found
 
-    /* check if sub index is valid */
-    if(subIndex >= object->maxSubIndex) {
-        return CO_SDO_AB_SUB_UNKNOWN;     /* Sub index does not exist  */
-    }
+    // Get object
+    object = &CO_OD[OD_entry_num];
 
-    /* figure out OD entry type */
-    if(object->maxSubIndex == 0U){    /* Object type is Var */
-        ODattribute = object->attribute;
-        ODlength = object->length;
-        ODdata = object->pData;
+    // Check if sub index is valid
+    if(sub_index >= object->maxSubIndex)
+        return false; // Sub index does not exist
+
+    // Figure out OD entry type
+    if(object->maxSubIndex == 0U) { // Object type is Variable
+        OD_attribute = object->attribute;
+        OD_length = object->length;
+        OD_data = object->pData;
     }
-    else if(object->attribute != 0U){ /* Object type is Array */
-        ODattribute = object->attribute;
-        if(subIndex == 0) {
-            /* First subIndex is readonly, it holds the length of Array */
-            ODattribute &= ~(CO_ODA_WRITEABLE | CO_ODA_RPDO_MAPABLE);
-            ODattribute |= CO_ODA_READABLE;
-            ODlength = sizeof(object->maxSubIndex);
-            memcpy(&object->maxSubIndex, ODdata, sizeof(object->maxSubIndex));
+    else if(object->attribute != 0U) { // Object type is Array
+        OD_attribute = object->attribute;
+        if(sub_index == 0) {
+            // First sub_index is readonly, it holds the length of Array
+            OD_attribute &= ~(CO_ODA_WRITEABLE | CO_ODA_RPDO_MAPABLE);
+            OD_attribute |= CO_ODA_READABLE;
+            OD_length = sizeof(object->maxSubIndex);
+            memcpy(&object->maxSubIndex, OD_data, sizeof(object->maxSubIndex));
         }
-        else { /* array data */
-            ODlength = object->length;
-            ODdata = ((int8_t*)object->pData) + ((subIndex-1) * object->length);
+        else { // array data
+            OD_length = object->length;
+            OD_data = ((int8_t*)object->pData) + ((sub_index-1) * object->length);
         }
     }
-    else{                            /* Object type is Record */
-        ODattribute = ((const CO_OD_entryRecord_t*)(object->pData))[subIndex].attribute;
-        ODlength = ((const CO_OD_entryRecord_t*)(object->pData))[subIndex].length;
-        ODdata = ((const CO_OD_entryRecord_t*)(object->pData))[subIndex].pData;
+    else { // Object type is Record
+        OD_attribute = ((const CO_OD_entryRecord_t*)(object->pData))[sub_index].attribute;
+        OD_length = ((const CO_OD_entryRecord_t*)(object->pData))[sub_index].length;
+        OD_data = ((const CO_OD_entryRecord_t*)(object->pData))[sub_index].pData;
     }
 
-    /* is object readable? */
-    if((ODattribute & CO_ODA_READABLE) == 0){
-        return CO_SDO_AB_WRITEONLY;     /* attempt to read a write-only object */
-    }
+    if((OD_attribute & CO_ODA_READABLE) == 0)
+        return false; // Attempted to read a write-only object
 
-    /* check if domain type */
-    if(ODdata != NULL){ /* not a domain type*/
-        CO_LOCK_OD();
+    if(OD_data == NULL)
+        return false; // Is a domain type
 
-        /* copy data */
-        *length = ODlength;
-        memcpy(data, ODdata, ODlength);
+    CO_LOCK_OD();
 
-        CO_UNLOCK_OD();
-    }
-    else { /* domain type */
-        //TODO call domain functions
-    }
+    // Copy data
+    *length = OD_length;
+    memcpy(data, OD_data, OD_length);
 
-    return 0;
+    CO_UNLOCK_OD();
+
+    return true;
 }
 
 
-/******************************************************************************/
-uint32_t app_writeOD(uint16_t index, uint16_t subIndex, void *data, uint16_t length){
+bool
+app_OD_write(uint16_t index, uint16_t sub_index, void *data, uint16_t length) {
     CO_OD_entry_t* object = NULL;
-    int8_t *ODdata = NULL;
-    uint16_t ODlength;
-    uint16_t ODattribute;
+    int8_t *OD_data = NULL;
+    uint16_t OD_entry_num;
+    uint16_t OD_length;
+    uint16_t OD_attribute;
 
-    if(data == NULL) {
-        return CO_SDO_AB_NO_DATA; 
+    if(data == NULL || length == 0)
+        return false; // No data to write
+
+    OD_entry_num = app_OD_find(index);
+    if(OD_entry_num == 0xFFFE)
+        return false; // Index not found
+
+    // Get object
+    object = &CO_OD[OD_entry_num];
+
+    if(sub_index >= object->maxSubIndex)
+        return false; // Sub index does not exist
+
+    // Figure out OD entry type
+    if(object->maxSubIndex == 0U) { //Object type is Varaible
+        OD_attribute = object->attribute;
+        OD_length = object->length;
+        OD_data = object->pData;
     }
-
-    uint16_t entryNo = app_OD_find(index);
-    if(entryNo == 0xFFFE) {
-        return CO_SDO_AB_NOT_EXIST;  /* index not found */
-    }
-
-    /* get object */
-    object = &CO_OD[entryNo];
-
-    /* check if sub index is valid */
-    if(subIndex >= object->maxSubIndex) {
-        return CO_SDO_AB_SUB_UNKNOWN;     /* Sub index does not exist  */
-    }
-
-    /* figure out OD entry type */
-    if(object->maxSubIndex == 0U){    /* Object type is Var */
-        ODattribute = object->attribute;
-        ODlength = object->length;
-        ODdata = object->pData;
-    }
-    else if(object->attribute != 0U){ /* Object type is Array */
-        ODattribute = object->attribute;
-        if(subIndex == 0) {
-            /* First subIndex is readonly, it holds the length of Array */
-            ODattribute &= ~(CO_ODA_WRITEABLE | CO_ODA_RPDO_MAPABLE);
-            ODattribute |= CO_ODA_READABLE;
-            ODlength = sizeof(object->maxSubIndex);
-            memcpy(ODdata, &object->maxSubIndex, sizeof(object->maxSubIndex));
+    else if(object->attribute != 0U) { // Object type is Array
+        OD_attribute = object->attribute;
+        if(sub_index == 0) {
+            // First sub_index is readonly, it holds the length of Array
+            OD_attribute &= ~(CO_ODA_WRITEABLE | CO_ODA_RPDO_MAPABLE);
+            OD_attribute |= CO_ODA_READABLE;
+            OD_length = sizeof(object->maxSubIndex);
+            memcpy(OD_data, &object->maxSubIndex, sizeof(object->maxSubIndex));
         }
-        else { /* array data */
-            ODlength = object->length;
-            ODdata = ((int8_t*)object->pData) + ((subIndex-1) * object->length);
+        else { // Array data
+            OD_length = object->length;
+            OD_data = ((int8_t*)object->pData) + ((sub_index-1) * object->length);
         }
     }
-    else{                            /* Object type is Record */
+    else { // Object type is Record
         CO_OD_entryRecord_t *temp = (CO_OD_entryRecord_t *)object->pData;
-        ODattribute = temp[subIndex].attribute;
-        ODlength = temp[subIndex].length;
-        ODdata = temp[subIndex].pData;
+        OD_attribute = temp[sub_index].attribute;
+        OD_length = temp[sub_index].length;
+        OD_data = temp[sub_index].pData;
     }
 
-    /* is object writeable? */
-    if((ODattribute & CO_ODA_WRITEABLE) == 0){
-        return CO_SDO_AB_READONLY;     /* attempt to write a read-only object */
-    }
+    if((OD_attribute & CO_ODA_WRITEABLE) == 0)
+        return false; // Attempted to write a read-only object
 
-    /* check if domain type */
-    if(ODdata != NULL){ /* not a domain type*/
-        if(length > ODlength) {
-            return CO_SDO_AB_TYPE_MISMATCH;     /* Length of service parameter does not match */
-        }
+    if(OD_data == NULL)
+        return false; // Is a domain type, can't write it
 
-        CO_LOCK_OD();
+    if(length > OD_length)
+        return false; // Length of service parameter does not match
 
-        /* copy data  */
-        ODlength = length;
-        memcpy(ODdata, data, length);
+    CO_LOCK_OD();
 
-        CO_UNLOCK_OD();
-    }
-    else { /* domain type */
-        //TODO call domain functions
-    }
+    // copy the data
+    OD_length = length;
+    memcpy(OD_data, data, length);
 
-    return 0;
+    CO_UNLOCK_OD();
+
+    return true;
 }
 
--- src/common/OD_helpers.h
+++ src/common/OD_helpers.h
@@ -2,13 +2,36 @@
 #define APP_OD_HELPERS_H
 
 
+#include "CANopen.h"
 #include <stdint.h>
 
 
 /**
- * Find the location in the OD array from the OD index value.
+ * A nice wrapper for apps to not worry about CO->SDO[].
+ */
+void app_OD_configure(
+        uint16_t index,
+        CO_SDO_abortCode_t (*pODFunc)(CO_ODF_arg_t *ODF_arg),
+        void *object,
+        uint8_t *flags,
+        uint8_t flagsSize);
+
+
+/**
+ * A nice function to log message in syslogs with a standard format.
  *
- * @param index OD index for OD entry
+ * @param app_name: for application name
+ * @param priority: for log proprity
+ * @param message: the message to log
+ */
+void app_log_message(const char *app_name, int priority, const char *message);
+
+
+/**
+ * Find the location in the OD array from the OD index value.
+ * Mostly a copy from CO_SDO.* that is for apps and not CANopenNode.
+ *
+ * @param index: OD index for OD entry
  *
  * @return array location or 0xFFFF for failure.
  */
@@ -16,29 +39,39 @@ uint16_t app_OD_find(uint16_t index);
 
 
 /**
- * Find the location in the OD array from the OD index value.
+ * Reads data into object dictionary.
+ * Mostly a copy from CO_SDO.* that is for apps and not CANopenNode.
  *
- * @param index OD index for OD entry
- * @param subIndex OD sub-index for OD entry
- * @param data Buffer for copy data from OD
- * @param length Length of data buffer in bytes
+ * @param index: OD index for OD entry
+ * @param subIndex: OD sub-index for OD entry
+ * @param data Buffer: for copy data from OD
+ * @param length: Length of data buffer in bytes
  *
- * @return 0 on success.
+ * @return true on success.
  */
-uint32_t app_readOD(uint16_t index, uint16_t subIndex, void *data, uint16_t *length);
+bool app_OD_read(
+        uint16_t index,
+        uint16_t subIndex,
+        void *data,
+        uint16_t *length);
 
 
 /**
- * Find the location in the OD array from the OD index value.
+ * Writes data into object dictionary.
+ * Mostly a copy from CO_SDO.* that is for apps and not CANopenNode.
  *
- * @param index OD index for OD entry
- * @param subIndex OD sub-index for OD entry
- * @param data Data buffer to copy into OD
- * @param length Length of data buffer in bytes
+ * @param index: OD index for OD entry
+ * @param subIndex: OD sub-index for OD entry
+ * @param data: Data buffer to copy into OD
+ * @param length: Length of data buffer in bytes
  *
- * @return 0 on success.
+ * @return true on success.
  */
-uint32_t app_writeOD(uint16_t index, uint16_t subIndex, void *data, uint16_t length);
+bool app_OD_write(
+        uint16_t index,
+        uint16_t subIndex,
+        void *data,
+        uint16_t length);
 
 
 #endif
