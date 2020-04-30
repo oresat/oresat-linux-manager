a48002c3d5961b32e468f44237f604a91a76e10b
ryanpdx <rmedick@pdx.edu>
2020-04-30 10:40:11 -0700

    move system_apps to their own directory

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -13,6 +13,7 @@ def Settings( **kwargs ):
             '-I./src/boards/template',
             '-I./src/boards/template/object_dictionary',
             '-I./src/common',
+            '-I./src/system_apps',
             '-I./src/socketCAN',
             '-I./src/CANopenNode',
             '-I./src/CANopenNode/stack',
