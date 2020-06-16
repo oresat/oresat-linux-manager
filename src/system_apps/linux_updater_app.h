657f0a50541dbf3a9476da40f0225825101b9b4c
ryanpdx <rmedick@pdx.edu>
2020-06-16 10:14:58 -0700

    merged OreSat Linux updater readme into app's doxygen comments

--- src/system_apps/linux_updater_app.h
+++ src/system_apps/linux_updater_app.h
@@ -25,6 +25,9 @@
  * @{
  *
  * System app that communicates with the OreSat Linux updater daemon.
+ * See the [oresat-linux-updater repo](https://github.com/oresat/oresat-linux-updater)
+ * for info the the Linux Updater Daemon.
+
  *
  * See <https://github.com/oresat/oresat-linux-updater> for info on the daemon.
  */
@@ -40,6 +43,25 @@ int linux_updater_dbus_signal_match(void);
 
 /**
  * @brief ODF callback for getting the linux updater's ODF.
+ *
+ * **Subindex of OD index:**
+ * | Sub Index | Usage                             | Data Type     | Access    |
+ * | :-------: | :-------------------------------: | :-----------: | :-------: |
+ * |     0     | Number of subindex in record      | uint8         | readonly  |
+ * |     1     | Update Status                     | uint8         | readonly  |
+ * |     2     | Current File                      | DOMAIN        | readonly  |
+ * |     3     | Updates Available                 | uint32        | readonly  |
+ * |     4     | Add Update File                   | DOMAIN        | writeonly |
+ * |     5     | Update Now                        | boolean       | writeonly |
+ * |     6     | Stop Update                       | boolean       | writeonly |
+ * |     7     | Delete Current File               | boolean       | writeonly |
+ *
+ * **How updates work:**
+ *  - The master CAN node can write in the paths to an update tar file for the updater to run in subindex 4. The updater will not run those updates until a boolean is written into subindex 5; at which point the updater will start or continue an update. If the master node need updater to stop, it can write a boolean to subindex 6.
+ *  - Subindex 1 is the current/last update status (negative for error, 0 for sleeping, 1 for updating, 2 for stopped).
+ *  - Subindex 2 and 3 is just useful information.
+ *  - Subindex 7 is for delete the update current update file in subindex 2 an any temp files make from uncompressed.
+ *
  *
  * @param ODF_arg Pointer to the current SDO ODF arguments
  *
