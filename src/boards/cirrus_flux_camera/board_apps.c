414e3446ffa6db4544b4d82a6728d9bd7e5e4c9c
ryanpdx <rmedick@pdx.edu>
2021-03-14 16:02:14 -0700

    remove very old tests

--- src/boards/cirrus_flux_camera/board_apps.c
+++ src/boards/cirrus_flux_camera/board_apps.c
@@ -1,15 +1,6 @@
 #include "apps.h"
-#include "pru_camera_test_app.h"
-
-#define PRU_CAMERA_TEST_ODF_INDEX  0x3100
-
-#define BOARD "cirrus_flux_camera" // TODO fix cmake
 
 int
 board_apps_setup() {
-
-    // pru camera test app
-    app_OD_configure(PRU_CAMERA_TEST_ODF_INDEX, PRU_CAMERA_TEST_ODF, NULL, 0, 0U);
-
     return 1;
 }
