49f36aadeeff2f9ee680eb2f1c194c7e90111071
Alexander DuPree <alexander.j.dupree@gmail.com>
2021-03-27 12:10:52 -0700

    Fixed malloc and strncpy macros behavior

--- src/common/utility.h
+++ src/common/utility.h
@@ -33,7 +33,7 @@ if (data != NULL) \
 
 
 #define MALLOC_AND_STRNCPY(out, in) \
-if ((out = malloc(strlen(in)+1)) == NULL) \
+if ((out = malloc(strlen(in)+1)) != NULL) \
     strncpy(out, in, strlen(in)+1);
 
 #define MALLOC_STRNCPY_OR_GOTO(out, in, goto_label) \
@@ -42,7 +42,7 @@ else \
     goto goto_label;
 
 #define MALLOC_AND_MEMCPY(out, in) \
-if ((out = malloc(sizeof(in))) == NULL) \
+if ((out = malloc(sizeof(in))) != NULL) \
     memcpy(out, in, sizeof(in));
 
 #define MALLOC_MEMCPY_OR_GOTO(out, in, goto_label) \
