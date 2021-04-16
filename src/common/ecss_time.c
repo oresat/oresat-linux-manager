72a954d56403daa98bf2adeed0452ed4508fec0f
ryanpdx <rmedick@pdx.edu>
2021-04-15 18:04:17 -0700

    added ecss time

---
+++ src/common/ecss_time.c
@@ -0,0 +1,54 @@
+#include "ecss_time.h"
+#include <stddef.h>
+#include <sys/time.h>
+
+#define SECS_IN_DAY (24*60*60)
+
+void get_time_scet(time_scet_t *scet) {
+    struct timeval time;
+
+    if (scet == NULL)
+        return;
+
+    gettimeofday(&time, NULL);
+
+    scet->coarse = time.tv_sec;
+    scet->fine = time.tv_usec;
+}
+
+void set_time_scet(const time_scet_t *scet) {
+    struct timeval time;
+
+    if (scet == NULL)
+        return;
+
+    time.tv_sec = scet->coarse;
+    time.tv_usec = scet->fine;
+
+    settimeofday(&time, NULL);
+}
+
+void get_time_utc(time_utc_t *utc) {
+    struct timeval time;
+
+    if (utc == NULL)
+        return;
+
+    gettimeofday(&time, NULL);
+
+    utc->day = time.tv_sec / SECS_IN_DAY;
+    utc->ms = time.tv_usec / 1000;
+    utc->us = time.tv_usec % 1000;
+}
+
+void set_time_utc(const time_utc_t *utc) {
+    struct timeval time;
+
+    if (utc == NULL)
+        return;
+
+    time.tv_sec = utc->day * SECS_IN_DAY;
+    time.tv_usec = utc->ms * 1000 + utc->us;
+
+    settimeofday(&time, NULL);
+}
---
+++ src/common/ecss_time.h
@@ -0,0 +1,38 @@
+#ifndef ECSS_TIME_H
+#define ECSS_TIME_H
+
+#include <stdint.h>
+#include <sys/time.h>
+
+/* CCSDS Unsegmented Time Code (CUC) data type */
+typedef union {
+    uint64_t raw;
+    struct {
+        uint32_t coarse:32;
+        uint32_t fine:24; // sub seconds
+    };
+} time_scet_t;
+
+/* CCSDS Day Segmented Time Code (CDS) data type */
+typedef union {
+    uint64_t raw;
+    struct {
+        uint16_t day:16;
+        uint32_t ms:32;
+        uint16_t us:16;
+    };
+} time_utc_t;
+
+/** Gets the local time in SCET format */
+void get_time_scet(time_scet_t *scet);
+
+/** Sets the local time from a SCET format */
+void set_time_scet(const time_scet_t *scet);
+
+/** Gets the local time in CCSDS UTC format */
+void get_time_utc(time_utc_t *utc);
+
+/** Sets the local time from a CCSDS UTC format */
+void set_time_utc(const time_utc_t *utc);
+
+#endif /*ECSS_TIME_H*/
